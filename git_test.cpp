bd27eb6c0f70bd52af3cdc63ad5c966d7d83bde3
张昊 <zh@git.buctsnc.cn>
2020-03-27 23:08:41 +0800

    add git test file

---
+++ git_test.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+/**
+* @author: Zhang Hao   
+*/
+void printZhangHao(){
+    std::cout << "张昊学会了用git！" << std::endl;
+}
+int main(){
+    printZhangHao();
+    return 0;
+}
